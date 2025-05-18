ca08dc4da0c59001e2ef2cfa3855ae08c37703b6
Georbots <mpotsoglou02@gmail.com>
2025-05-18 17:44:19 +0300

    modularity

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-class Perceptron:
-    def __init__(self):
-        pass  # initialize weights, bias, learning rate here
-
-    def predict(self, x):
-        pass  # code for forward pass
-
-    def train(self, X, y):
-        pass  # code for training loop
+print('hello world')
---
+++ perceptron.py
@@ -0,0 +1,11 @@
+import numpy
+
+class Perceptron:
+    def __init__(self):
+        pass  # initialize weights, bias, learning rate here
+
+    def predict(self, x):
+        pass  # code for forward pass
+
+    def train(self, X, y):
+        pass  # code for training loop
