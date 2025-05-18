e2436ee3f576791849202a6f91000ba92078b134
Georbots <mpotsoglou02@gmail.com>
2025-05-18 17:43:01 +0300

    create Perceptron class

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Perceptron:
+    def __init__(self):
+        pass  # initialize weights, bias, learning rate here
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def predict(self, x):
+        pass  # code for forward pass
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def train(self, X, y):
+        pass  # code for training loop
