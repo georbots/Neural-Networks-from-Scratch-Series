c9a95527bb0eec1ff06709846311b7cea8e8a1e2
Georbots <mpotsoglou02@gmail.com>
2025-05-25 17:09:19 +0300

    add data_utils

--- 2.batch_training/main.py
+++ 2.batch_training/main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Seed for reproducibility
 np.random.seed(42)
 
 # Generate 150 points around (1,1) for class 0
@@ -10,13 +9,11 @@ class0 = np.random.randn(150, 2) + np.array([1, 1])
 # Generate 150 points around (4,4) for class 1
 class1 = np.random.randn(150, 2) + np.array([4, 4])
 
-# Stack the data together
-X = np.vstack((class0, class1))
 
-# Create labels: 0 for class0, 1 for class1
+X = np.vstack((class0, class1))
 y = np.hstack((np.zeros(150), np.ones(150)))
 
-# Plot
+
 plt.figure(figsize=(8,6))
 plt.scatter(class0[:, 0], class0[:, 1], color='red', label='Class 0')
 plt.scatter(class1[:, 0], class1[:, 1], color='blue', label='Class 1')
